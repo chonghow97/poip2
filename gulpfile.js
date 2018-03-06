ec3cb668563337c39d0cfe425c49c0ac54bcc8f3
Chong How <chong.how97@live.com>
2018-03-06 15:17:22 +0800

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+//sass to css
+gulp.task('sass',function(){
+	return gulp.src('./sass/*.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.stream());
+});
+
+
+
+// Jade to html
+gulp.task('jade',function () {
+	gulp.src('./jade/*.jade')
+	.pipe(jade({
+		pretty: true
+	}))
+	.pipe(gulp.dest('./html'))
+	.pipe(browserSync.stream());
+})
+
+gulp.task('serve', function () {
+    browserSync.init({
+        proxy: 'localhost:80',
+        port: '7777'
+    });
+    gulp.watch("./*.php").on("change", reload);
+	gulp.watch('./jade/*jade',['jade']);
+	gulp.watch('./sass/*sass', ['sass']);
+});
+
+// watch Task
+gulp.task('watch',function () {
+	gulp.watch("./*.php").on("change", reload);
+	gulp.watch('./jade/*jade',['jade']);
+	gulp.watch('./sass/*sass', ['sass']);
+});
+
+//Default
+gulp.task('default',['jade','sass','watch','serve']);
