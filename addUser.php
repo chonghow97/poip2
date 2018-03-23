29edd298e9487a248717b8a22bcd846eed2f79b6
Chong How <chong.how97@live.com>
2018-03-23 08:53:17 +0800

    minior fixed corner branding ahref

--- addUser.php
+++ addUser.php
@@ -1,12 +1,12 @@
-<?php 
-	// require_once 'conn.php'
-	// if(isset($_POST['add'])){
-	// 	$fullname =
-	// 	$password = 
-	// 	$email = 
-	// 	$role = 
-	// }
+<?php
+	echo "<body></body>";
+	require_once 'conn.php';
+	if(isset($_POST['add'])){
+		$fullname = $_POST['fname'];
+		//$password = auto generate 6 digit
+		$email = $_POST['email'];
+		$role = $_POST['role'];
+	}
+	
 	require './html/addUser.html';
-	$rand = substr(md5(microtime()),rand(0,26),6);
-	echo $rand;
  ?>
