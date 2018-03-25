5648c5df5ae709c90ff3ff18582a91393672eccf
Chong How <chong.how97@live.com>
2018-03-25 11:09:11 +0800

    sql update

--- addItem.php
+++ addItem.php
@@ -1,3 +1,22 @@
-<?php 
+<?php
+	echo "<body></body>";
+	require_once 'conn.php';
+	$query = "SELECT * FROM category";
+	$result= mysqli_query($link,$query);
+	$sql = "SELECT * FROM measure";
+	$result1 = mysqli_query($link,$sql);
+		if(isset($_POST['add'])){
+			$item = $_POST['name'];
+			$category = $_POST['category'];
+			$measure = $_POST['measure'];
+			$status = $_POST['status'];
+			$query1 = "INSERT INTO item VALUES(null,'$item','$category','$measure','$status')";
+			if($itemresult = mysqli_query($link,$query1)){
+				echo "<script>alert('added Successfully');</script>";
+				$_POST = array();
+			}else{
+				echo mysqli_error($link);
+			}
+		}
 	require './html/additem.html';
  ?>
