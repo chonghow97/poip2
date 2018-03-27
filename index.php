164c679a02e8ced95b66a8c39562385526559532
Chong How <chong.how97@live.com>
2018-03-27 20:16:47 +0800

    fixed session login

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	session_start();
 	require_once 'conn.php';
 	if(isset($_POST['sub'])){
-		$email = $_POST['uemail'];
+		$email = htmlentities($_POST['uemail']);
 		$password = $_POST['upassword'];
 		$sql = "SELECT fname, password, role, firstlogin FROM user WHERE email='$email'";
 		if($result = mysqli_query($link,$sql)){
@@ -10,13 +10,13 @@
 				$row= mysqli_fetch_assoc($result);
 				$hpass= $row['password'];
 				if(password_verify($password,$hpass)){
+					$_SESSION['islogin'] = true;
+					$_SESSION['loginUsername'] = $row['fname'];
 					if($row['role']){
 						header('Location: '.'user.php');
 					}else{
 						header('Location: '.'dashboard.php');
 					}
-					$_SESSION['islogin'] = true;
-					$_SESSION['loginUsername'] = $row['fname'];
 				}else{
 					echo "<script>alert('Wrong password please try again')</script>";
 				}
